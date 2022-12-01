1d8624ac2e5ed23f9a2b2c08f447e3f64bb93723
Loïc Nogues <nogues.loic@gmail.com>
2022-12-01 14:29:10 +0100

    Installed tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import "./App.css";
 import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Pokedex</h1>
+    <div className="">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <SearchForm />
     </div>
   );
